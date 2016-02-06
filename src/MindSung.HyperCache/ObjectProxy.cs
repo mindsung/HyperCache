d7b2e2bd4a267d6b44421b6528c95a7ada6715d7
robb@mindsung.com <robb@mindsung.com>
2016-02-06 12:35:04 -0500

    Added implementation stub for ObjectProxy

---
+++ src/MindSung.HyperCache/ObjectProxy.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MindSung.HyperCache
+{
+    public class ObjectProxy<T> where T : class, IEnumerable<KeyValuePair<string, object>>
+    {
+        public ObjectProxy(T value)
+        {
+            if (value == null) { throw new ArgumentException("ObjectProxy value cannot be null", "value"); }
+        }
+
+        public ObjectProxy(string serialized)
+        {
+            if (serialized == null) { throw new ArgumentException("ObjectProxy serialized value cannot be null", "serialized"); }
+        }
+
+        object sync = new object();
+
+        public T Value
+        {
+            get
+            {
+                if (_Value == null)
+                {
+                    lock (sync)
+                    {
+                        if (_Value == null)
+                        {
+                            // TODO: Deserialize
+                        }
+                    }
+                }
+                return _Value;
+            }
+        }
+        T _Value = null;
+
+        public string Serialized
+        {
+            get
+            {
+                if (_Serialized == null)
+                {
+                    lock (sync)
+                    {
+                        if (_Serialized == null)
+                        {
+                            // TODO: Serialize
+                        }
+                    }
+                }
+                return _Serialized;
+            }
+        }
+        string _Serialized = null;
+    }
+}
