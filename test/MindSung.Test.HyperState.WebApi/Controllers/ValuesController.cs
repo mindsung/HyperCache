37d9d163e2f04a868bdbfa3f26979a002c82c17e
Robb Eads <robb@eadsfamily.us>
2017-01-03 00:21:29 -0500

    Improved web api test controller

--- test/MindSung.Test.HyperState.WebApi/Controllers/ValuesController.cs
+++ test/MindSung.Test.HyperState.WebApi/Controllers/ValuesController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using MindSung.HyperState;
 using MindSung.HyperState.AspNetCore;
 using System.Diagnostics;
+using System.Collections.Concurrent;
 
 namespace MindSung.Test.HyperState.WebApi.Controllers
 {
@@ -16,59 +17,77 @@ namespace MindSung.Test.HyperState.WebApi.Controllers
         {
             this.factory = factory;
 
-            var val1 = factory.FromObject(new MyValues { IntValue = 1234, StringValue = "hello" });
-            var val2 = factory.FromObject(new MyValues { IntValue = 2345, StringValue = "there" });
-            myValues[val1.Object.IntValue] = val1;
-            myValues[val2.Object.IntValue] = val2;
+            if (simpleCache == null)
+            {
+                simpleCache = new ConcurrentDictionary<int, string>();
+                var val1 = factory.FromObject(new MyValues { Id = 1234, Value = "hello" });
+                var val2 = factory.FromObject(new MyValues { Id = 2345, Value = "there" });
+                simpleCache[val1.Object.Id] = val1.Serialized;
+                simpleCache[val2.Object.Id] = val2.Serialized;
+            }
         }
 
         IWebObjectProxyFactory<string> factory;
 
         public class MyValues
         {
-            public int IntValue { get; set; }
-            public string StringValue { get; set; }
+            public int Id { get; set; }
+            public string Value { get; set; }
         }
 
-        static Dictionary<int, IObjectProxy<MyValues, string>> myValues = new Dictionary<int, IObjectProxy<MyValues, string>>();
+        static ConcurrentDictionary<int, string> simpleCache;
 
         // GET api/values
         [HttpGet]
         public ActionResult Get()
         {
-            return Ok(myValues.Values);
+            return Ok(simpleCache.Values.Select(s => factory.FromSerialized<MyValues>(s)));
         }
 
         // GET api/values/{id}
         [HttpGet("{id}")]
         public ActionResult Get(int id)
         {
-            IObjectProxy<MyValues, string> val;
-            if (!myValues.TryGetValue(id, out val))
+            string s;
+            if (!simpleCache.TryGetValue(id, out s))
             {
                 return NotFound();
             }
-            return Ok(val);
+            return Ok(factory.FromSerialized<MyValues>(s));
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]IObjectProxy<MyValues, string> proxy)
+        public ActionResult Post([FromBody]IObjectProxy<MyValues, string> proxy)
         {
-            myValues[proxy.Object.IntValue] = proxy;
+            if (!simpleCache.TryAdd(proxy.Object.Id, proxy.Serialized))
+            {
+                throw new Exception("An item with the specified ID already exists.");
+            }
+            return Ok(proxy);
         }
 
         // PUT api/values/{id}
         [HttpPut("{id}")]
         public ActionResult Put(int id, [FromBody]IObjectProxy<MyValues, string> proxy)
         {
-            if (!myValues.ContainsKey(id))
+            if (!simpleCache.ContainsKey(id))
             {
                 return NotFound();
             }
-            var val = proxy.Object;
-            myValues[val.IntValue] = proxy;
-            return Ok();
+            if (id != proxy.Object.Id)
+            {
+                throw new Exception("The request update ID doesn't match that of the item.");
+            }
+            simpleCache[proxy.Object.Id] = proxy.Serialized;
+            return Ok(proxy);
+        }
+
+        // POST api/values/put/{id}
+        [HttpPost("put/{id}")]
+        public ActionResult PostPut(int id, [FromBody]IObjectProxy<MyValues, string> proxy)
+        {
+            return Put(id, proxy);
         }
 
         // PATCH api/values
@@ -77,7 +96,7 @@ namespace MindSung.Test.HyperState.WebApi.Controllers
         {
             foreach (var proxy in values)
             {
-                myValues[proxy.Object.IntValue] = proxy;
+                simpleCache[proxy.Object.Id] = proxy.Serialized;
             }
             return Ok(values);
         }
@@ -93,11 +112,19 @@ namespace MindSung.Test.HyperState.WebApi.Controllers
         [HttpDelete("{id}")]
         public ActionResult Delete(int id)
         {
-            if (!myValues.Remove(id))
+            string _;
+            if (!simpleCache.TryRemove(id, out _))
             {
                 return NotFound();
             }
             return Ok();
+        }
+
+        // GET api/values/delete/{id}
+        [HttpGet("delete/{id}")]
+        public ActionResult GetDelete(int id)
+        {
+            return Delete(id);
         }
     }
 }
