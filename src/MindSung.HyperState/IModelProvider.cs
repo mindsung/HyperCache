da7cba67dab5f669cdb210f267f144c6f899873a
Robb Eads <robb@eadsfamily.us>
2017-01-21 14:54:51 -0500

    Basic framework for HyperStateModel

---
+++ src/MindSung.HyperState/IModelProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MindSung.HyperState
+{
+    public interface IModelProvider<TEntity, TId, TSerialized>
+    {
+        Task<IObjectProxy<TEntity, TSerialized>> Insert(IObjectProxy<TEntity, TSerialized> value);
+        Task<IObjectProxy<TEntity, TSerialized>> Update(TId id, IObjectProxy<TEntity, TSerialized> value);
+        Task<IObjectProxy<TEntity, TSerialized>> Get(TId id);
+        Task Delete(TId id);
+    }
+}
